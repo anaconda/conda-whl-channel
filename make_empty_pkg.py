cffcf0b7204e4e6f33df74c308057a55c0cc65cd
Jonathan Helmus <jjhelmus@gmail.com>
2025-01-16 10:40:57 -0600

    add make_empty_pkg.py

---
+++ make_empty_pkg.py
@@ -0,0 +1,14 @@
+import io
+import tarfile
+
+filename = "sample-2.0-0.tar.bz2"
+content = {
+    "info/index.json": b'{}',
+    "info/paths.json": b'{"paths":[],"paths_version":1}',
+}
+
+with tarfile.open(filename, mode="w:bz2") as tf:
+    for name, payload in content.items():
+        ti = tarfile.TarInfo(name)
+        ti.size = len(payload)
+        tf.addfile(ti, io.BytesIO(payload))
